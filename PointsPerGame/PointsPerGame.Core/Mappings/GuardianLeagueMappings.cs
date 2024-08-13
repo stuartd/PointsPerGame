9234e68afad27b7a8a4432a0237aed51a411b9f0
stuart <stuartd@gmail.com>
2024-08-13 17:22:21 +0100

    Had to switch to https URLS, which caused some breakage which took a while to figure out

--- PointsPerGame/PointsPerGame.Core/Mappings/GuardianLeagueMappings.cs
+++ PointsPerGame/PointsPerGame.Core/Mappings/GuardianLeagueMappings.cs
@@ -6,43 +6,43 @@ namespace PointsPerGame.Core.Mappings {
 		public static string GetUriForLeague(League league) {
 			switch (league) {
 				case League.EnglishPremierLeague:
-					return "http://www.theguardian.com/football/premierleague/table";
+					return "https://www.theguardian.com/football/premierleague/table";
 
 				case League.EnglishChampionship:
-					return "http://www.theguardian.com/football/championship/table";
+					return "https://www.theguardian.com/football/championship/table";
 
 				case League.EnglishLeagueOne:
-					return "http://www.theguardian.com/football/leagueonefootball/table";
+					return "https://www.theguardian.com/football/leagueonefootball/table";
 
 				case League.EnglishLeagueTwo:
-					return "http://www.theguardian.com/football/leaguetwofootball/table";
+					return "https://www.theguardian.com/football/leaguetwofootball/table";
 
 				case League.WSL:
 					return "https://www.theguardian.com/football/womens-super-league/table";
 
 				case League.SPL:
-					return "http://www.theguardian.com/football/scottish-premiership/table";
+					return "https://www.theguardian.com/football/scottish-premiership/table";
 
 				case League.ScottishChampionship:
-					return "http://www.theguardian.com/football/scottish-championship/table";
+					return "https://www.theguardian.com/football/scottish-championship/table";
 
 				case League.ScottishLeagueOne:
-					return "http://www.theguardian.com/football/scottish-league-one/table";
+					return "https://www.theguardian.com/football/scottish-league-one/table";
 
 				case League.ScottishLeagueTwo:
-					return "http://www.theguardian.com/football/scottish-league-two/table";
+					return "https://www.theguardian.com/football/scottish-league-two/table";
 
 				case League.LaLiga:
-					return "http://www.theguardian.com/football/laligafootball/table";
+					return "https://www.theguardian.com/football/laligafootball/table";
 
 				case League.Ligue1:
-					return "http://www.theguardian.com/football/ligue1football/table";
+					return "https://www.theguardian.com/football/ligue1football/table";
 
 				case League.Bundesliga:
-					return "http://www.theguardian.com/football/bundesligafootball/table";
+					return "https://www.theguardian.com/football/bundesligafootball/table";
 
 				case League.SeriaA:
-					return "http://www.theguardian.com/football/serieafootball/table";
+					return "https://www.theguardian.com/football/serieafootball/table";
 
 				case League.AllTopDivisions:
 					return null;
--- PointsPerGame/PointsPerGame.Core/Web/Scraper.cs
+++ PointsPerGame/PointsPerGame.Core/Web/Scraper.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace PointsPerGame.Core.Web {
@@ -9,6 +10,9 @@ namespace PointsPerGame.Core.Web {
 
 		static Scraper() {
 			httpClient = new HttpClient();
+			httpClient.DefaultRequestHeaders.Accept.Clear();
+			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html")); // Or another media type
+			httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0");
 		}
 
 		protected static async Task<Stream> GetPageStreamAsync(string url) {
@@ -16,7 +20,7 @@ namespace PointsPerGame.Core.Web {
 			var uri = new Uri(url);
 			ServicePointManager.FindServicePoint(uri);
 
-			var response = await httpClient.GetAsync(new Uri(url));
+			var response = await httpClient.GetAsync(uri);
 
 			response.EnsureSuccessStatusCode();
 
